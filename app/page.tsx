6e9aec88917c2c01fc9f41f58602fe2b115ec70c
Naman Sharma <namansharmans03@gmail.com>
2026-04-27 20:58:27 +0530

    button

--- app/page.tsx
+++ app/page.tsx
@@ -10,15 +10,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-neutral-100 ">
-    <HeroBG/>
-<BunnyIcon state="sleeping" />    // z's float, ears sway, body breathes
-<BunnyIcon state="awake" />       // eyes snap open, ears perk
-<BunnyIcon state="surprised" />   // wide eyes
-<BunnyIcon state="success" />     // wink
-
-// as a loading indicator:
-<BunnyIcon state={isLoading ? "sleeping" : "success"} size={24} color="rgba(200,184,154,0.7)" />
-<SmoothButton />
+      <BunnyIcon />
     </div>
   );
 }
